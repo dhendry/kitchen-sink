1d640171cf8bc480799ac250062880b856fe0391
Dan Hendry <hendry.dan@gmail.com>
2018-03-01 15:40:37 -0500

    Moar messing around with proto

--- frivolosity/frivolosity.go
+++ frivolosity/frivolosity.go
@@ -1,7 +1,83 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
 func main() {
-  fmt.Println("Hello World!")
+	var someNumber int
+	{
+		someNumber = rand.Int()
+	}
+	fmt.Printf("Hello World! %d\n", someNumber)
+
+	for i := 0; i < 5; i++ {
+		fmt.Printf("Normal %d\n", i)
+		defer fmt.Printf("Defer %d\n", i)
+	}
+
+	t1 := new(SomeType)
+	t1.f1 = 3
+
+	fmt.Printf("Foo: %d\n", t1.f1)
+
+	t2 := SomeType{f1: 4}
+	fmt.Printf("Foo2: %d\n", t2.f1)
+
+	t2.f1 = 5
+	fmt.Printf("Foo3: %d\n", t2.f1)
+
+	//t2 = &SomeType{f1: 6}
+	//fmt.Printf("Foo3: %d\n", t2.f1)
+
+	DoSomething(&t2)
+	fmt.Printf("Foo4: %d\n", t2.f1)
+
+	DoSomething(&t2)
+	fmt.Printf("Foo4: %d\n", t2.f1)
+
+	t2.PointerReceiver()
+	fmt.Printf("After PointerReceiver: %s\n", t2)
+
+	t2.ValueReceiver()
+	fmt.Printf("After ValueReceiver: %s\n", t2)
+
+	var i1 FooInterface
+	i1 = &t2
+	fmt.Printf("After interface cast: %s\n", i1)
+
+	i1.PointerReceiver()
+	fmt.Printf("After PointerReceiver: %s\n", t2)
+}
+
+// Also note the use of multiple variabls
+func nakedReturn(a, b int) (multiplyResult, addResult int) {
+	multiplyResult = a * b
+	addResult = a + b
+	return
+}
+
+type SomeType struct {
+	f1 int
+	f2 string
+}
+
+func DoSomething(st *SomeType) () {
+	st.f1 = 7
+	fmt.Printf("DoSomething: %s\n", st)
+}
+
+func (st *SomeType) PointerReceiver() () {
+	st.f1 = 8
+	fmt.Printf("PointerReceiver: %s\n", st)
+}
+
+func (st SomeType) ValueReceiver() () {
+	st.f1 = 9
+	fmt.Printf("ValueReceiver: %s\n", st)
+}
+
+type FooInterface interface {
+	PointerReceiver()
 }
---
+++ frivolosity/frivolosity_test.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNakedReturn(t *testing.T) {
+	fmt.Printf("TestNakedReturn\n")
+
+	mult, add := nakedReturn(5, 6)
+
+	if mult != 30 {
+		t.Fail()
+	}
+
+	if add != 11 {
+		t.Fail()
+	}
+}
+
+func TestMaps(t *testing.T) {
+	fmt.Printf("TestMaps\n")
+
+	m := map[string]bool{
+		"java": false,
+		"go":   true,
+	}
+
+	assert.Equal(t, true, m["go"])
+	assert.Equal(t, false, m["java"])
+	assert.Equal(t, false, m["fooo"])
+
+	v, ok := m["fooo"]
+	assert.Equal(t, false , v)
+	assert.Equal(t, false , ok)
+
+}
+
+func getAccumulator() func(int) int {
+	var sum int
+	return func(i int) int {
+		sum += i
+		return sum
+	}
+}
+
+func TestAccumulator(t *testing.T) {
+	acc := getAccumulator()
+	assert.Equal(t, 1 , acc(1))
+	assert.Equal(t, 2 , acc(1))
+	assert.Equal(t, 4 , acc(2))
+}
+
+
+func TestNil(t *testing.T) {
+	var v1 *SomeType
+	assert.True(t, v1.IsNil())
+
+	v1 = &SomeType{}
+	assert.False(t, v1.IsNil())
+
+}
+
+func (st *SomeType) IsNil() bool {
+	return st == nil
+}
